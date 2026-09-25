cc38fa6286178b40d2096609bf4cb7008cb8b64d
RakanDhiya <ramydhiya2@gmail.com>
2026-09-25 10:22:58 +0700

    bugfix: remove comment in dashboard

--- resources/views/private/dashboard.blade.php
+++ resources/views/private/dashboard.blade.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Dashboard | Smart Worklog</title>
-
-    {{-- Tailwind CSS --}}
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
